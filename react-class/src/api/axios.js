a891111fe99db3dcb01f082e34eb41e7a864a5fb
martinsfrmdass <akpanmartins999@gmail.com>
2026-09-02 09:56:19 +0100

    added link

--- react-class/src/api/axios.js
+++ react-class/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "https://backend-batch-9.vercel.app/api",
+    baseURL: "https://56d45766-013d-48e4-aa35-8aef7e06a88f-00-3ctmccbbw7dbi.picard.replit.dev/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
